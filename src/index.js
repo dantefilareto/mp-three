d619aaa706db79ed6b5d009f2e67f0df7bea2e0b
Dante Filareto <filaretodante@gmail.com>
2022-10-03 15:53:02 -0300

    remove node modules

--- src/index.js
+++ src/index.js
@@ -3,11 +3,10 @@ const mpService = require('./mp-service');
 const app = express(); 
 const port = 5000;
 
+const prefIds = mpService.init();
 app.get('/', (req, res) => {
-    res.sendFile('index.html', {root: `${__dirname}/html`});                                       
+    res.sendFile('index.html?pref1=', {root: `${__dirname}/html`});                                       
 });
-
 app.listen(port, () => {          
     console.log(`Now listening on port ${port}`); 
 });
-mpService.init();
--- src/mp-service.js
+++ src/mp-service.js
@@ -5,6 +5,23 @@ exports.init = async function(){
     mercadopago.configure({
         access_token: "TEST-200312321772452-051315-eef217da54a82d0436a9a7f33244ef30-303364636",
       });
-    await mercadopago.preferences.create(preference.preferenceEbookTest1)
-    await mercadopago.preferences.create(preference.preferenceVideoTest1)
+    //await mercadopago.preferences.create(preference.preferenceEbookTest1)
+    //await mercadopago.preferences.create(preference.preferenceVideoTest1)
+
+    mercadopago.preferences
+    .create(preference.preferenceEbookTest1)
+    .then(function (response) {
+        response.body.id
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+    mercadopago.preferences
+    .create(preference.preferenceVideoTest1)
+    .then(function (response) {
+        response.body.id
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
 }
